e5eccaa4b69c721351a877b57e1c2a83059134f3
Jean-Philippe Bergeron <bergeron.jeanphilippe@gmail.com>
2018-08-05 21:43:10 -0400

    First test

--- test/Simple.test.js
+++ test/Simple.test.js
@@ -1,47 +1,13 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import styled from 'styled-components';
+import 'jest-styled-components';
 
-const App = styled.div`
-	color: @text;
-`;
-
-// const Title = styled.h1`
-// 	font-size: 1.5em;
-// 	color: @highlight;
-// 	b {
-// 		color: lighten(@highlight, 20%);
-// 	}
-// `;
-//
-// const Wrapper = styled.section`
-// 	text-align: center;
-// 	padding: 4em;
-// 	background-color: @background;
-// `;
-//
-// const Button = styled.a`
-// 	cursor: pointer;
-// 	cursor: if(@disabled, not-allowed);
-// 	color: white;
-// 	padding: 10px 30px;
-// 	border: 1px solid rgba(0, 0, 0, 0.21);
-// 	border-bottom: 4px solid rgba(0, 0, 0, 0.21);
-// 	border-radius: 4px;
-// 	text-shadow: 0 1px 0 rgba(0, 0, 0, 0.15);
-// 	background: linear-gradient(@highlight 0%, darken(@highlight, 5%) 100%);
-// 	&:active {
-// 		background: darken(@highlight, 10%);
-// 	}
-// `;
-
-test( 'Link changes the class when hovered', () => {
-	const App = styled.div`
-		color: @text;
+test( 'Div takes the color from props', () => {
+	const Div = styled.div`
+		background-color: chocolate;
+		color: @primary;
 	`;
-	const component = renderer.create(
-		<App>Facebook</App>,
-	);
-	let tree = component.toJSON();
+	const tree = renderer.create( <Div primary="red">Facebook</Div> ).toJSON()
 	expect( tree ).toMatchSnapshot();
 } );
