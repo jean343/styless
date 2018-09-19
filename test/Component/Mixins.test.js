a25936cbd0064fed897dbf070b1851ad7f4f2a31
Aiden Rourke <arourke@interset.com>
2018-09-19 15:40:21 -0400

    Mixin guards

--- test/Component/Mixins.test.js
+++ test/Component/Mixins.test.js
@@ -18,3 +18,37 @@ test('Support inline mixins', () => {
 	`;
     expect(renderer.create(<Div color="red" background="white"/>).toJSON()).toMatchSnapshot();
 });
+
+test.skip('Test mixin guards 1', () => {
+	const Div = styled.div`
+        .theme (@mode) when (@mode = "dark") {
+        	background-color: darkblue;
+        }
+        .theme (@mode) when (@mode = "light") {
+        	background-color: lightblue;
+        }
+        .theme (@mode) when (default()) {
+  			background-color: @mode;
+		}
+  		.theme(@scheme);
+    `;
+	expect(renderer.create(<Div scheme="dark"/>).toJSON()).toMatchSnapshot();
+});
+
+
+test.skip('Test mixin guards 2', () => {
+	const Div = styled.div`
+		.mixin(@a) when (lightness(@a) >= 50%) {
+		  background-color: black;
+		}
+		.mixin(@a) when (lightness(@a) < 50%) {
+		  background-color: white;
+		}
+		.mixin(@a) {
+		  color: @a;
+		}
+		.class1 { .mixin(#ddd) }
+		.class2 { .mixin(#555) }
+    `;
+	expect(renderer.create(<Div/>).toJSON()).toMatchSnapshot();
+});
