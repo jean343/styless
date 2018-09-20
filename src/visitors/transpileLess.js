670a553c237249a30ba675c9278c556ac53d3d46
Jean-Philippe Bergeron <bergeron.jeanphilippe@gmail.com>
2018-09-19 21:34:42 -0400

    Support template literals in ifs

--- src/visitors/transpileLess.js
+++ src/visitors/transpileLess.js
@@ -129,19 +129,20 @@ export default (source, filename) => {
     less.tree.Negative = Negative;
     less.tree.Dimension = Dimension;
     less.tree.Expression.prototype.genCSS = function (context, output) {
-        const isVariable = this.value.some(v => v instanceof VariableNode);
-        if (isVariable) output.add('`');
-        for (let value of this.value) {
-            if (value instanceof VariableNode) {
+        const cssFragment = context.cssFragment && this.value.some(v => v instanceof VariableNode);
+        if (cssFragment) output.add('`');
+        for (let i = 0; i < this.value.length; i++) {
+            const value = this.value[i];
+            if (cssFragment && value instanceof VariableNode) {
                 output.add(`\${${value.value}}`);
             } else {
                 value.genCSS(context, output);
             }
-            if (!(this.noSpacing || value.noSpacing)) {
+            if (!(this.noSpacing || value.noSpacing) && i + 1 < this.value.length) {
                 output.add(' ');
             }
         }
-        if (isVariable) output.add('`');
+        if (cssFragment) output.add('`');
     };
     less.tree.Anonymous.prototype.eval = function () {
         const anonymous = new less.tree.Anonymous(this.value, this._index, this._fileInfo, this.mapLines, this.rulesetLike, this.visibilityInfo());
