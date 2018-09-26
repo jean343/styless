829489cd16887cefa63f8cbc001dc6845f10ba9d
Jean-Philippe Bergeron <bergeron.jeanphilippe@gmail.com>
2018-09-26 10:39:35 -0400

    fix(import): Add support for base imports (#26)
    * Add a base import
    
    It will prepend to all components
    @import (reference) "${opts.import}";
    
    * Allow imports from the root folder.

---
+++ test/Component/BaseImportAbsolute/foo/DirImport.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import styled from 'styled-components';
+import 'jest-styled-components';
+
+test('It should find the base import in sub folders', () => {
+    const Div = styled.div`
+		color: @base-color;
+		.base-mixin;
+	`;
+    expect(renderer.create(<Div/>).toJSON()).toMatchSnapshot();
+});
