0b8285ff912190898a273f20ca39871ea621f629
Aiden Rourke <38865246+aidenrrourke@users.noreply.github.com>
2019-01-20 15:04:32 -0500

    Adding mixin tests (#28)
    * Mixin guards
    
    * Mixin guard tests
    
    * Added isUnit test

--- test/Component/Mixins.test.js
+++ test/Component/Mixins.test.js
@@ -18,3 +18,36 @@ test('Support inline mixins', () => {
 	`;
     expect(renderer.create(<Div color="red" background="white"/>).toJSON()).toMatchSnapshot();
 });
+
+test.skip('Test mixin guards', () => {
+	const Div = styled.div`
+		.mixin(@a) when (lightness(@a) >= 50%) {
+		  background-color: black;
+		}
+		.mixin(@a) when (lightness(@a) < 50%) {
+		  background-color: white;
+		}
+		.mixin(@a) {
+		  color: @a;
+		}
+		.class1 { .mixin(#ddd) }
+		.class2 { .mixin(#555) }
+    `;
+	expect(renderer.create(<Div/>).toJSON()).toMatchSnapshot();
+});
+
+// test.skip('Test Less loops', () => {
+// 	const Div = styled.div`
+// 		.make-variants(@i:1) when (@i =< 3) {
+// 			.variant-@{i} {
+// 				width: @i * 40px;
+// 				height: @i * 20px;
+// 				background-color: orange;
+// 				margin-bottom: 10px;
+// 			}
+// 		  .make-variants(@i + 1); // increment function
+// 		}
+// 		.make-variants();
+// 	`;
+// 	expect(renderer.create(<Div/>).toJSON()).toMatchSnapshot();
+// });
